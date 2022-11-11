279e98316ae72d71c32315a5d7be6bc5d5669c23
M Sohail <60110179+sohail151127@users.noreply.github.com>
2022-11-11 22:01:15 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 // import './App.css';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import MovieDetail from "./pages/MovieDetails";
+import Login from "./pages/logIn/Login";
+import Register from "./pages/register/Register";
 
-import SignUp from "./components/SignUp";
+
+import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState("")
   return (
-    <div className="App">
-      <SignUp />
+    <div>
+      <Navbar />
+      
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Register" element={<Register />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/MovieDetail/:id" element={<MovieDetail />} />
+    </Routes>
+
     </div>
   );
 }
